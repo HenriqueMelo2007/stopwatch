0c0a17200929bade7f9557e1ae71efeb27208b0a
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-06-28 15:09:57 -0300

    feat: it builds the program

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char const *argv[])
 {
+  clock_t startTime, endTime;
+
+  double elapsedTime;
+
+  startTime = clock();
+
+  printf("Timer started. Press any key to stop.\n");
+  getchar();
+
+  endTime = clock(); 
+  elapsedTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+
+  printf("Elapsed time: %.2f seconds.\n", elapsedTime);
   
   return 0;
 }
